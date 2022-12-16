bea4af26a7bf33a64875a3dc738e4f36daf5c877
jloescher <jonathan@jloescher.com>
2022-12-15 23:33:18 -0500

    Corrected logic on the match statement to avoid having to enter no two times to exit and save the destination.

--- main.py
+++ main.py
@@ -1,39 +1,67 @@
 import random
 
-# (5 points): As a developer, I want to make at least three commits with descriptive messages 
+# (5 points): As a developer, I want to make at least three commits with descriptive messages
 
 # (5 points):  As a developer, I want to store my destinations, restaurants, mode of transportation, and entertainment in their own separate Lists.
 destinations = ["New York", "Tampa", "Miami", "Chicago", "Atlanta", "San Francisco"]
-restaurants = [["Kochi", "Crown Shy", "Dhamaka", "Gage & Tollner"], ["Vino E Pasta", "Happy Fish", "Wright's Gourmet House", "Terra Gaucha Brazilian Steakhouse Tampa"], ["Stubborn Seed", "Boia De", "La Mar", "The Surf Club Restaurant"], ["Superdawg Drive-In", "Luella's Southern Kitchen", "Bayan Ko", "Community Tavern"], ["Spring Restaurant", "Minas Grill & Emporium", "Virgil's Gullah Kitchen & Bar", "Heirloom Market BBQ"], ["Abacá", "Lucca Delicatessen", "Sotto Mare", "Restaurant Nisei"]]
+restaurants = [
+    ["Kochi", "Crown Shy", "Dhamaka", "Gage & Tollner"],
+    [
+        "Vino E Pasta",
+        "Happy Fish",
+        "Wright's Gourmet House",
+        "Terra Gaucha Brazilian Steakhouse Tampa",
+    ],
+    ["Stubborn Seed", "Boia De", "La Mar", "The Surf Club Restaurant"],
+    ["Superdawg Drive-In", "Luella's Southern Kitchen", "Bayan Ko", "Community Tavern"],
+    [
+        "Spring Restaurant",
+        "Minas Grill & Emporium",
+        "Virgil's Gullah Kitchen & Bar",
+        "Heirloom Market BBQ",
+    ],
+    ["Abacá", "Lucca Delicatessen", "Sotto Mare", "Restaurant Nisei"],
+]
 transportation = ["train", "bus", "plane", "boat"]
-entertainment = ["concert", "theatre", "sight seeing", "cultural sites", "historic sites"]
+entertainment = [
+    "concert",
+    "theatre",
+    "sight seeing",
+    "cultural sites",
+    "historic sites",
+]
 
 # (5 points): As a user, I want a destination to be randomly selected for my day trip.
 session_destination = ""
 
+
 def random_destination():
-  random = random.randint(0, len(destinations))
-  return destinations[random]
+    random_number = random.randint(0, len(destinations))
+    return destinations[random_number]
+
 
 def destination_sequence():
-  session_destination = random_destination()
-  print(f"Your random destination is {session_destination}")
-  destination_confirm = input(f"Are you happy with {session_destination} as your destination or would you like to choose another destination? Y or N")
-  return destination_confirm.lower()
+    session_destination = random_destination()
+    print(f"Your random destination is {session_destination}")
+    destination_confirm = input(
+        f"Are you happy with {session_destination} as your destination or would you like to choose another destination? Y or N: "
+    )
+    return destination_confirm.lower()
+
 
 running = True
 while running:
-  destination_confirm = destination_sequence()
-  match destination_confirm:
-    case "y":
-      destination_confirm = destination_sequence()
-    case "n":
-      session_destination = session_destination
-      break
+    destination_confirm = destination_sequence()
+    match destination_confirm:
+        case "y":
+            continue
+        case "n":
+            session_destination = session_destination
+            break
 
 # (5 points): As a user, I want a restaurant to be randomly selected for my day trip
 
-# (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip. 
+# (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip.
 
 # (5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.
 
@@ -43,4 +71,4 @@ while running:
 
 # (10  points): As a user, I want to display my completed trip in the console
 
-# (5 points): Single Responsibility: As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing! 
+# (5 points): Single Responsibility: As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!
